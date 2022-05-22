540fa3116962905fb70527f059df9beb56c4d515
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2022-05-22 20:27:25 +0200

    setup bitcoinjslib hack

---
+++ mobile/ios/Pods/Target Support Files/Pods-WalletPOC-WalletPOCTests/Pods-WalletPOC-WalletPOCTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WalletPOC_WalletPOCTests : NSObject
+@end
+@implementation PodsDummy_Pods_WalletPOC_WalletPOCTests
+@end
---
+++ mobile/ios/Pods/Target Support Files/Pods-WalletPOC/Pods-WalletPOC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WalletPOC : NSObject
+@end
+@implementation PodsDummy_Pods_WalletPOC
+@end
