1015b8ed454443b0b6835a7f90fc18bd395a389c
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2022-05-26 21:35:42 +0200

    testet to reimport created wallet

--- mobile/app/views/create-wallet/create-wallet.tsx
+++ mobile/app/views/create-wallet/create-wallet.tsx
@@ -15,17 +15,17 @@ const masterSeed = '000102030405060708090a0b0c0d0e0f';
 //uuid - server/client
 //const uuid = 'e4a1dfc6-88de-408d-a00e-14672d004a5d';
 
+const uuidTest = 'adf21dea-30d2-49b6-93df-51d999fc718d'
+const masterSeedTest = '000102030405060708090a0b0c0d0e0f';
+const userSeedTest = 'E9873D79C6D87DC0FB6A5778633389';
+
 function getAddress(node: any, network?: any): string {
   return bitcoin.payments.p2pkh({ pubkey: node.publicKey, network }).address!;
 }
 
 function splitUuid(uuid: string): string {
   const uuidNew = uuid.split('-');
-  var dec0 = parseInt(uuidNew[0],16).toString();
-  var dec1 = parseInt(uuidNew[1],16).toString();
-  var dec2 = parseInt(uuidNew[2],16).toString();
-  var dec3 = parseInt(uuidNew[3],16).toString();
-  return dec1+"'/"+dec2+"'/"+dec3+"'";
+  return uuidNew[0]+uuidNew[1]+uuidNew[2]+uuidNew[3]+uuidNew[4];
 }
 
 function hmacSHA512(key: Buffer, data: string): Buffer {
@@ -42,30 +42,41 @@ const CreateWallet = () => {
   const [userSeed, SetUserSeed] = useState<String>();
   const [combinedSeed, SetCombinedSeed] = useState<String>();
   const [clientRoot, SetClientRoot] = useState<String>();
+  const [uuidSeed, SetUUID] = useState<String>();
 
   const createAddress = () => {
 
-    //master root - server
+    //master root - server - safe
     const masterRoot = fromSeed(
       Buffer.from(
-        masterSeed,
+        masterSeedTest,
         'hex',
       ),
     );
     SetMasterRoot(masterRoot.toBase58())
 
+    const uuid4 = uuid.v4().toString()
+    SetUUID(uuid4)
+
+    console.log(splitUuid(uuid4))
+
+
     //serverSeed - server
-    const child1 = masterRoot.derivePath(splitUuid(uuid.v4().toString()))
-    const serverSeed = Buffer.from(child1.privateKey as Buffer).toString('hex')+Buffer.from(child1.chainCode).toString('hex');
-    SetServerSeed(serverSeed)
+    const serverSeed = hmacSHA512(Buffer.from(splitUuid(uuidTest)), Buffer.from(masterRoot.privateKey as Buffer).toString('hex'))
+    //const child1 = masterRoot.derivePath(splitUuid(uuid.v4().toString()))
+    //const serverSeed = Buffer.from(child1.privateKey as Buffer).toString('hex')+Buffer.from(child1.chainCode).toString('hex');
+    SetServerSeed(serverSeed.toString('hex'))
 
     //userSeed - client
     const keyPair = bitcoin.ECPair.makeRandom();
-    const userSeed = Buffer.from(keyPair.privateKey as Buffer).toString('hex');
+    //const userSeed = Buffer.from(keyPair.privateKey as Buffer).toString('hex');
+    const userSeed = userSeedTest
     SetUserSeed(userSeed)
 
     //combined user/server-Seed - client at initial setup
-    const combinedSeed = hmacSHA512(Buffer.from('Bitcoin seed', 'utf8'), userSeed+serverSeed);
+    //const combinedSeed = hmacSHA512(Buffer.from('Bitcoin seed', 'utf8'), userSeed+serverSeed);
+    //combine with threshold signature
+    const combinedSeed = hmacSHA512(serverSeed, userSeed);
     SetCombinedSeed(Buffer.from(combinedSeed as Buffer).toString('hex'));
         
 
@@ -78,18 +89,19 @@ const CreateWallet = () => {
     );
     SetClientRoot(clientRoot.toBase58())
 
-    Alert.alert("user acc address: " + getAddress(clientRoot.derivePath("m/44'/0'/0'/0'/0'")))
+    Alert.alert("user acc address: " + getAddress(clientRoot))
   }
 
   return( 
     <View style={styles.container}>
         <Text style={styles.text}>MasterSeed: {masterSeed}</Text>
-       <Pressable 
-        onPress={() => createAddress()}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Receive Address</Text>
-      </Pressable>
+        <Pressable 
+          onPress={() => createAddress()}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Receive Address</Text>
+        </Pressable>
       {masterRoot && <Text>masterroot xpriv: {masterRoot} {'\n'} </Text>}
+      {uuidSeed && <Text>uuid: {uuidSeed}</Text>}
       {serverSeed && <Text>serverSeed: {serverSeed} {'\n'} </Text>}
       {userSeed && <Text>userSeed: {userSeed} {'\n'} </Text>}
       {combinedSeed && <Text>combinedSeed: {combinedSeed} {'\n'} </Text>}
--- mobile/app/views/import-wallet/import-wallet.tsx
+++ mobile/app/views/import-wallet/import-wallet.tsx
@@ -1,8 +1,99 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from 'react';
+import {Text, useColorScheme, View, Pressable, Alert, TextInput} from 'react-native';
+import {  fromBase58, fromSeed,   } from 'bip32';
+import uuid from 'react-native-uuid';
+const createHmac = require('create-hmac');
+import "@Shim";
+import * as bitcoin from "bitcoinjs-lib";
 
-const ImportWallet = () => {
-  return <Text>Now you can import your wallet</Text>;
+import { getStyles } from './import-wallet.styles';
+
+function getAddress(node: any, network?: any): string {
+  return bitcoin.payments.p2pkh({ pubkey: node.publicKey, network }).address!;
+}
+
+function splitUuid(uuid: string): string {
+  const uuidNew = uuid.split('-');
+  return uuidNew[0]+uuidNew[1]+uuidNew[2]+uuidNew[3]+uuidNew[4];
+}
+
+function hmacSHA512(key: Buffer, data: string): Buffer {
+  return createHmac('sha512', key)
+      .update(data)
+      .digest();
+}
+
+const CreateWallet = () => {
+  const styles = getStyles(useColorScheme() === 'dark')
+
+  const [masterSeed, onChangeMasterSeed] = React.useState("MasterSeed");
+  const [uuid, onChangeUUID] = React.useState("UUID");
+  const [userSeed, onChangeUserSeed] = React.useState("UUID");
+  
+  
+
+  const [masterRoot, SetMasterRoot] = useState<String>();
+  const [serverSeed, SetServerSeed] = useState<String>();
+  const [combinedSeed, SetCombinedSeed] = useState<String>();
+  const [clientRoot, SetClientRoot] = useState<String>();
+
+  const recoverAddress = () => {
+
+
+    //master root - server - safe
+    const masterRoot = fromSeed(
+      Buffer.from(
+        masterSeed,
+        'hex',
+      ),
+    );
+    SetMasterRoot(masterRoot.toBase58())
+
+    //serverSeed - server
+    const serverSeed = hmacSHA512(Buffer.from(splitUuid(uuid)), Buffer.from(masterRoot.privateKey as Buffer).toString('hex'))
+    //const child1 = masterRoot.derivePath(splitUuid(uuid.v4().toString()))
+    //const serverSeed = Buffer.from(child1.privateKey as Buffer).toString('hex')+Buffer.from(child1.chainCode).toString('hex');
+    SetServerSeed(serverSeed.toString('hex'))
+
+    //combined user/server-Seed - client at initial setup
+    //const combinedSeed = hmacSHA512(Buffer.from('Bitcoin seed', 'utf8'), userSeed+serverSeed);
+    //combine with threshold signature
+    const combinedSeed = hmacSHA512(serverSeed, userSeed);
+    SetCombinedSeed(Buffer.from(combinedSeed as Buffer).toString('hex'));
+        
+
+    //client extended private key
+    const clientRoot = fromSeed(
+      Buffer.from(
+        Buffer.from(combinedSeed as Buffer).toString('hex'),
+        'hex',
+      ),
+    );
+    SetClientRoot(clientRoot.toBase58())
+
+    Alert.alert("user acc address: " + getAddress(clientRoot))
+  }
+
+  return( 
+    <View style={styles.container}>
+        <Text>MasterSeed - hard save at server</Text>
+        <TextInput style={styles.input} onChangeText={onChangeMasterSeed} />
+        <Text>UUID - saved at user</Text>
+        <TextInput style={styles.input} onChangeText={onChangeUUID} />
+        <Text>UserSeed - save at user</Text>
+        <TextInput style={styles.input} onChangeText={onChangeUserSeed} />
+        <Pressable 
+          onPress={() => recoverAddress()}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Recover Address</Text>
+        </Pressable>
+      {masterRoot && <Text>masterroot xpriv: {masterRoot} {'\n'} </Text>}
+      {serverSeed && <Text>serverSeed: {serverSeed} {'\n'} </Text>}
+      {combinedSeed && <Text>combinedSeed: {combinedSeed} {'\n'} </Text>}
+      {clientRoot && <Text>clientroot xpriv: {clientRoot} {'\n'} </Text>}
+
+    </View>
+  );
 };
 
-export default ImportWallet;
+export default CreateWallet;
